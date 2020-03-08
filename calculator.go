3effe98a130c5452aba09c605d5b5e1e33f921a9
David Liu <msglight4874@gmail.com>
2020-03-08 18:18:03 +0800

    Implement combo case in `CalBooksPrice`

--- calculator.go
+++ calculator.go
@@ -2,19 +2,68 @@ package calculator
 
 const price = 100.0
 
-func CalBooksPrice(books []int) float64 {
-	if len(books) == 2 {
+func getPriceByDiffCount(diffCount int) float64 {
+	switch diffCount {
+	case 2:
 		return price * 2 * 0.95
-	}
-	if len(books) == 3 {
+	case 3:
 		return price * 3 * 0.9
-	}
-	if len(books) == 4 {
+	case 4:
 		return price * 4 * 0.8
-	}
-	if len(books) == 5 {
+	case 5:
 		return price * 5 * 0.75
+	default:
+		return price
+	}
+}
+
+func buildBookCount(books []int) map[int]int {
+	bookCount := map[int]int{}
+	for _, book := range books {
+		count, ok := bookCount[book]
+		if ok {
+			bookCount[book] = count + 1
+		} else {
+			bookCount[book] = 1
+		}
 	}
 
-	return price
+	return bookCount
+}
+
+func buildDiffCounts(bookCount map[int]int) []int {
+	diffCounts := []int{}
+
+	for {
+		diffCount := 0
+		for key, count := range bookCount {
+			if count > 0 {
+				bookCount[key] -= 1
+				diffCount += 1
+			}
+
+			if diffCount == 5 {
+				break
+			}
+		}
+		if diffCount > 0 {
+			diffCounts = append(diffCounts, diffCount)
+		} else {
+			break
+		}
+	}
+
+	return diffCounts
+}
+
+func CalBooksPrice(books []int) float64 {
+	bookCount := buildBookCount(books)
+	diffCounts := buildDiffCounts(bookCount)
+
+	finalPrice := 0.0
+	for _, diffCounts := range diffCounts {
+		finalPrice += getPriceByDiffCount(diffCounts)
+	}
+
+	return finalPrice
 }
--- calculator_test.go
+++ calculator_test.go
@@ -19,11 +19,29 @@ func TestCalBooksPrice(t *testing.T) {
 			books []int
 			want  float64
 		}{
+			{"Buy [] book", []int{}, 0.0},
 			{"Buy [1] book", []int{1}, price},
 			{"Buy [1, 2] book", []int{1, 2}, price * 2 * 0.95},
 			{"Buy [1, 2, 3] books", []int{1, 2, 3}, price * 3 * 0.9},
 			{"Buy [1, 2, 3, 4] book", []int{1, 2, 3, 4}, price * 4 * 0.8},
 			{"Buy [1, 2, 3, 4, 5] book", []int{1, 2, 3, 4, 5}, price * 5 * 0.75},
+		}
+		for _, example := range examples {
+			t.Run(example.name, func(t *testing.T) {
+				assertBooksPrize(t, example.books, example.want)
+			})
+		}
+	})
+
+	t.Run("Combo cases", func(t *testing.T) {
+		examples := []struct {
+			name  string
+			books []int
+			want  float64
+		}{
+			{"Buy [1, 1] book", []int{1, 1}, price * 2},
+			{"Buy [1, 2, 4] book", []int{1, 2, 4}, price * 3 * 0.9},
+			{"Buy [1, 2, 4, 5] book", []int{1, 2, 4, 5}, price * 4 * 0.8},
 		}
 		for _, example := range examples {
 			t.Run(example.name, func(t *testing.T) {
